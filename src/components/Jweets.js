5bdba471cc560c836a020b9695c5c9610702f6bf
JwaSangHeon <whk4051@naver.com>
2022-04-25 13:20:37 +0900

    삭제버튼 구현

--- src/components/Jweets.js
+++ src/components/Jweets.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { dbService } from "../fbase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 const Jweets = ({ jweet, isOwner }) => {
+  const onClickDelete = async () => {
+    const ok = window.confirm("정말로 삭제하시겠습니까?");
+    console.log(ok);
+    const jweetTextRef = doc(dbService, "Jweets", `${jweet.id}`);
+    if (ok) {
+      await deleteDoc(jweetTextRef);
+    } else {
+      //noDelete
+    }
+  };
   return (
     <div>
       <h4>{jweet.text}</h4>
       {isOwner && (
         <>
-          <button>Delete</button>
+          <button onClick={onClickDelete}>Delete</button>
           <button>Edit</button>
         </>
       )}
